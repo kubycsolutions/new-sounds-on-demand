536dc807111782498b52f4a0ef959249919d2d9a
Joe Kesselman <keshlam@alum.mit.edu>
2021-10-26 14:54:31 -0400

    ditto

---
+++ src/config.prod.js
@@ -0,0 +1,18 @@
+// config.prod.js is a "staging" configuration -- specifically, for
+// production we want to override from using FileDb (easier to debug)
+// to DynamoDb (more efficient, available in Lambda environment)
+//
+// By referencing process.env, we are able to pick up the table name
+// from the runtime environment.
+//
+// TODO REVIEW: If I wanted to move the episodes.json file into tables, what
+// would we have to do?
+
+module.exports = {
+    db: {
+        DynamoDb: {
+            // tableName: process.env.DB_TABLE_NAME,
+	    tableName: "NewSoundsUserStates",
+        }
+    }
+};
