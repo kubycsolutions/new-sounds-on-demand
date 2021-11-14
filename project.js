e058e786e09fabc5cf5c5c5bf8808046c009a70c
Joe Kesselman <keshlam@alum.mit.edu>
2021-11-13 20:25:52 -0500

    There are some advantages to moving from DialogFlow to Google's new Conversational Action platform; it reports more progress/status info on the audio playback. There are also disadvantages, such as it not supporting a duration slot-type. Drop hooks in now, decide later.

--- project.js
+++ project.js
@@ -18,9 +18,10 @@ module.exports = {
       },
    },
    googleAction: {
-      nlu: 'dialogflow',
+       // nlu: 'dialogflow',
+       projectId: `new-sounds-on-demand`,
    },
-   // endpoint: '${JOVO_WEBHOOK_URL}',
+   // endpoint: '${JOVO_WEBHOOK_URL}', // talk to 'jovo run' instance
     host: {
 	lambda: {
             arn: 'arn:aws:lambda:us-east-1:046935287063:function:prod-new-sounds-on-demand',
