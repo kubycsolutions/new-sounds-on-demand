e72341f587968441614c8bb6169c0dccd871733f
Joe Kesselman <keshlam@alum.mit.edu>
2021-12-12 14:58:09 -0500

    We *should* be able to automatically switch between endpoints based on the Stage setting of the build. Don't quite have that working automatically yet, so I'm switching the config manually as needed.

--- project.js
+++ project.js
@@ -22,18 +22,27 @@ module.exports = {
 	projectId: `new-sounds-on-demand`,
     },
 
-    endpoint: '${JOVO_WEBHOOK_URL}',
+//    endpoint: '${JOVO_WEBHOOK_URL}',
+    host: {
+    	lambda: {
+    	    arn: 'arn:aws:lambda:us-east-1:046935287063:function:prod-new-sounds-on-demand',
+    	    askProfile: 'admin', // if left out: "default" profile is used
+    	}
+    }
     
     // In production, the Jovo code will run on an AWS Lambda
-    stages: {
-	prod: {
-	    endpoint: null,
-	    host: {
-    		lambda: {
-		    arn: 'arn:aws:lambda:us-east-1:046935287063:function:prod-new-sounds-on-demand',
-		    askProfile: 'admin', // if left out: "default" profile is used
-    		}
-	    }
-	}
-    }
+    // stages: {
+    // 	dev: {
+    // 	    endpoint: '${JOVO_WEBHOOK_URL}',
+    // 	},
+    // 	production: {
+    // 	    endpoint: null,
+    // 	    host: {
+    // 		lambda: {
+    // 		    arn: 'arn:aws:lambda:us-east-1:046935287063:function:prod-new-sounds-on-demand',
+    // 		    askProfile: 'admin', // if left out: "default" profile is used
+    // 		}
+    // 	    }
+    // 	}
+    // }
 };
