9c8bf30bb8a3671e1364f6580581d0a7601e5b40
Joe Kesselman <keshlam@alum.mit.edu>
2022-03-18 21:25:44 -0400

    Expedient, though wrong, solution: To produce two lambdas from a single codebase, I just have two entry points (index.ts and update_index.ts) and set the lambda's config appropriately. The update_index entrypoint is triggered by an EventBridge chron job, thus running our scheduled updates. Worked when invoked manually...

---
+++ src/update_index.ts
@@ -0,0 +1,30 @@
+import {createTable, waitForTable,
+       updateEpisodes
+} from "./episodesdb"
+
+// ------------------------------------------------------------------
+// Scheduled database update lambda entry point
+// ABSOLUTELY MINIMAL IMPLEMENTATION -- hardwired, no params, just
+// kick off an update.
+// ------------------------------------------------------------------
+
+const table=process.env.NSOD_EPISODES_TABLE || "episodes_debug"
+const maxdepth=-1 // // 0 to force rebuild, < incremental, > to specified depth
+
+// GONK: PARAMETERIZE. Endpoint and program should be selectable; currently
+// hardwired. Also, need to think about whether to start
+// loading/searching multiple shows to include Soundcheck, since it's
+// the other "recorded live" part of NewSounds... which will require
+// parameterizing the station-database URI since I don't think there's
+// an all-shows query thereupon. (Though their API *is* mostly
+// undocumented, so it may have more flexibility than I've been able
+// to access. Ideal, of course, would be to be able to run the
+// smartspeaker app directly against their data, but the part of the
+// API I've figured out isn't flexibile enough for more than paged
+// access.)
+
+// Minimal AWS Lambda entry point
+export const handler = async (event: any, context: any, callback: Function) => {
+    await updateEpisodes(table,maxdepth)
+    return "OK"
+};
